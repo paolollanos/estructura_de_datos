1467a50b0a7ef881697778866908524c365e6b8a
paolollanos <89824752+paolollanos@users.noreply.github.com>
2021-09-10 14:11:53 -0500

    Add files via upload

---
+++ Adt/adtarray.py
@@ -0,0 +1,22 @@
+from _typeshed import Self
+
+
+class Array():
+    def __init__(self, tamano):
+        self.__tamano = tamano
+        self.__tamano = [0 for x in range(self.__tamano)]
+    def get_item(self, index):
+        return self._data[index]
+    def to_string(self):
+        print(self.__data)
+
+    def setitem(dato, index):
+        index.append(dato)
+
+    def getlenght(self):
+        
+        self.__gettamano =[0 for n in range(self.__gettamano)]
+    
+    def clear(self, dato):
+        self.__dato = dato
+        self.__dato = [self == dato]
