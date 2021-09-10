7544ed9c6610b74075dcbd26a585e3b26e260692
paolollanos <89824752+paolollanos@users.noreply.github.com>
2021-09-10 14:13:52 -0500

    Add files via upload

---
+++ recursiones/clase6sep.py
@@ -0,0 +1,41 @@
+
+#recursividad
+
+def factorial(n):
+    resultado = 0 
+    if n == 0:
+        return 1
+    else:
+        return n * factorial( n - 1 )
+
+n = 3
+print(factorial(n))
+
+
+def printRev(n):
+    if n > 0 :
+        print(n)
+        printRev( n - 1 )
+
+n = 3
+printRev( n )
+
+def bomba(n):
+    if n > 0:
+        print(n)
+        bomba(n - 1 )
+    else:
+        print("Booom!!")
+n = 3
+print(bomba(n))
+
+
+lista = ["tengo hambre", 2, 2, "son cuatro", "y", 4, 16]
+def puntomedio(lista):
+    #if len(puntomedio - 1) == len(puntomedio)
+    if len(lista) > ((len(lista))/2):   
+        lista.pop((len(lista))-1)
+        puntomedio(lista)
+        print(lista)
+    
+print(puntomedio(lista))
